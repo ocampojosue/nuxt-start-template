9179c46f7431161e0e5b404619e67f28f149234e
ocampojosue <ocampojosue08@gmail.com>
2024-10-26 11:27:30 -0400

    add modeule animate on scroll

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="w-full h-screen bg-joj-800 flex justify-center items-center">
-    <h2 class="text-3xl text-white font-semibold">Tailwind is Working</h2>
+    <h2 class="text-3xl text-white font-semibold" data-aos="slide-up">Tailwind is Working</h2>
   </div>
 </template>
