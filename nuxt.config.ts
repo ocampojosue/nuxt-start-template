af5bf506ac1e15464b809f4ca59358f25af4eec7
ocampojosue <ocampojosue08@gmail.com>
2024-10-26 11:34:36 -0400

    fix slide and add config aos in nuxt ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-aos']
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-aos'],
+  aos: {
+    once: true
+  }
 })
